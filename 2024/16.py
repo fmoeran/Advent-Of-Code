e30573383ce3390fe9c6c7abfbd7b945c53937b7
felix <felixmoeran@gmail.com>
2024-12-26 18:20:01 +0000

    Improve 2024 day 16

--- 2024/16.py
+++ 2024/16.py
@@ -1,5 +1,6 @@
 # quite disappointed I didn't have to use anything more clever than just keeping track of all possible previous nodes
 # during a dijkstras, but oh well it works (slowly)!
+# UPDATE: added backtracking to improve speed.
 from heapq import heapify, heappop, heappush
 from collections import defaultdict
 
@@ -21,8 +22,6 @@ heap = [(0, start, 0)]
 scores = defaultdict(lambda: 1000000000000)
 scores[(start, 0)] = 0
 
-prevs = defaultdict(lambda: set())
-
 heapify(heap)
 bscore = 1000000000000000
 while heap:
@@ -33,30 +32,31 @@ while heap:
     npos = (pos[0] + d[0], pos[1] + d[1])
     if grid[npos[0]][npos[1]] != "#":
         if scores[(npos, d_ind)] >= score + 1:
-            if scores[(npos, d_ind)] == score+1:
-                for val in prevs[(pos, d_ind)]:
-                    prevs[(npos, d_ind)].add(val)
-            else:
-                prevs[(npos, d_ind)] = prevs[(pos, d_ind)].copy()
-            prevs[(npos, d_ind)].add(pos)
             heappush(heap, (score + 1, npos, d_ind))
             scores[(npos, d_ind)] = score + 1
 
     for nd in [(d_ind + 1) % 4, (d_ind - 1) % 4]:
         if scores[(pos, nd)] >= score + 1000:
-            if scores[(pos, nd)] == score+1000:
-                for val in prevs[(pos, d_ind)]:
-                    prevs[(pos, nd)].add(val)
-            else:
-                prevs[(pos, nd)] = prevs[(pos, d_ind)].copy()
-            prevs[(pos, nd)].add(pos)
             heappush(heap, (score + 1000, pos, nd))
             scores[(pos, nd)] = score + 1000
 
+
+def backtrack(pos, d_ind):
+    out = {pos}
+    d = difs[d_ind]
+    npos = (pos[0] - d[0], pos[1] - d[1])
+    current_score = scores[(pos, d_ind)]
+    if grid[npos[0]][npos[1]] != '#':
+        if scores[(npos, d_ind)] == current_score - 1:
+            out = out.union(backtrack(npos, d_ind))
+    for nd in [(d_ind + 1) % 4, (d_ind - 1) % 4]:
+        if scores[(pos, nd)] == current_score - 1000:
+            out = out.union(backtrack(pos, nd))
+    return out
+
+
 print(bscore)
-best = set()
 for d in range(4):
     if scores[(end, d)] == bscore:
-        for p in prevs[(end, d)]:
-            best.add(p)
-print(len(best)+1)
+        b = backtrack(end, d)
+        print(len(b))
